fe3f0926623aa694fcfa66bf58b361892043b7fa
PhilipTrauner <philip.trauner@aol.com>
2015-08-22 02:06:20 +0200

    Fixed setup

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
 	name='iTunesConnector',
-	version='4.0',
-	py_modules=['iTunesConnector', 'iTunes', 'States', 'Playlist', 'Track', 'FileTrack', 'Artwork', 'Window', 'AirplayDevices'],
+	version='4.1',
+	packages=["iTunesConnector"],
 	author="Philip Trauner",
 	author_email="philip.trauner@aol.com",
 	url="https://github.com/PhilipTrauner/iTunesConnector"
