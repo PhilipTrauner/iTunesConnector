8ada173244678a5cb9e30e00c03b9e67579c2c26
PhilipTrauner <philip.trauner@aol.com>
2015-08-23 22:19:26 +0200

    Revert "Fixed setup (for real this time) and introduced Python 3 support"
    This reverts commit 250232dff7070421d939556205e384db539e63f4.

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup
-from sys import version
-
-python_version = version[:3]
-if python_version not in ("3.3", "3.4", "2.7"):
-	print("ScriptingBridge (as of version 3.0.4) might not work with '%s'" % (python_version))
 
 setup(
 	name='iTunesConnector',
-	version='4.2',
+	version='4.1',
 	packages=["iTunesConnector"],
 	author="Philip Trauner",
-	install_requires=["pyobjc-framework-ScriptingBridge>=3.0.4"],
 	author_email="philip.trauner@aol.com",
 	url="https://github.com/PhilipTrauner/iTunesConnector"
 )
